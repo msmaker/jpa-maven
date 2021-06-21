3baf5a9527ae734a8acf0d6e1e9eeccf89852668
Marco Silva <marcosilva.tec@gamil.com>
2021-06-21 13:33:40 -0300

    DB Access

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,20 +1,36 @@
 package aplicacao;
 
-import entity.Pessoa;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1, "Marco", "marco@gmail.com");
-		Pessoa p2 = new Pessoa(1, "Dayana", "Daya@gmail.com");
-		Pessoa p3 = new Pessoa(1, "Alexandre", "aLe@gmail.com");
-		Pessoa p4 = new Pessoa(1, "Aline", "aline@gmail.com");
-		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-		System.out.println(p4);
+		Pessoa p1 = new Pessoa(null, "Marco", "marco@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Dayana", "Daya@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Alexandre", "aLe@gmail.com");
+		Pessoa p4 = new Pessoa(null, "Aline", "aline@gmail.com");
+
+		// gerando a comunicação com o banco de dados
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		// necessario iniciar uma transação
+		em.getTransaction().begin();
+
+		// persistindo ou salvando informação no banco de dados
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+
+		// efetuando a transação
+		em.getTransaction().commit();
+		System.out.println("Pronto");
+
 	}
 
 }
--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -1,11 +1,24 @@
-package entity;
+package dominio;
 
-public class Pessoa {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
-	
+
 	public Pessoa() {
 	}
 
@@ -43,6 +56,5 @@ public class Pessoa {
 	public String toString() {
 		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + "]";
 	}
-	
-	
+
 }
